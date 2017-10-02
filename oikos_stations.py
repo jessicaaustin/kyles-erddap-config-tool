2e556bb7f843aaf02f2bcae117cf71ff3dc4e12e
Kyle Wilcox <kyle@axiomdatascience.com>
2017-10-01 23:56:29 -0400

    Exit correctly

--- oikos_stations.py
+++ oikos_stations.py
@@ -150,7 +150,4 @@ if __name__ == "__main__":
                         default=False)
     args = parser.parse_args()
 
-    try:
-        sys.exit(main(args.output, args.region, args.link))
-    except BaseException:
-        sys.exit(1)
+    sys.exit(main(args.output, args.region, args.link))
